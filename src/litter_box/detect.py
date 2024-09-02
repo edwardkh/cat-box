4a6ee11d111768cc5d502a79575a818cfd648fda
edwardkh <edwardwang@gmail.com>
2024-09-02 09:09:02 -0700

    locked out cat detection during move due to false sensor activations.  increased sleep oop to .25 for anti crash

--- src/litter_box/detect.py
+++ src/litter_box/detect.py
@@ -2,7 +2,7 @@ from litter_box import sense, state
 
 
 def cat():
-    if sense.cat_in_the_box():
+    if state.get_state() == state.IDLE and sense.cat_in_the_box():
         state.cat_detected()
     if state.get_state() == state.PAUSED and state.ok_to_move():
         state.unpause()
